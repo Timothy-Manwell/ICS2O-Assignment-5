afb44312588dff04ab41cc796184e47867efff7a
Timothy Manwell <113055850+Timothy-Manwell@users.noreply.github.com>
2022-12-22 21:05:15 -0500

    init

--- js/script.js
+++ js/script.js
@@ -15,9 +15,16 @@ if (navigator.serviceWorker) {
   })
 }
 
-/**
- * This function displays an alert.
- */
 function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+  const timesThroughLoop =  parseInt(document.getElementById("times-through-loop").value)
+  var counter = 0
+  var piAnswer = 0
+
+  while (counter < timesThroughLoop) {
+    piAnswer = piAnswer + ((4 / (1 + (2 *counter))) * (-1) ** counter)
+    counter++
+  }
+  
+  document.getElementById("answer").innerHTML =
+    "Pi to " + timesThroughLoop + " digits is " + piAnswer + "."
 }
